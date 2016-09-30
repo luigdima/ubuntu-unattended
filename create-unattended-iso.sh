9b1a409b54aee19ff7b5d11243dfa992f2179236
Luis Ignacio Díaz Martínez <luigdima@gmail.com>
2016-09-30 14:00:18 +0200

    Update create-unattended-iso.sh

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-# file names & paths
-tmp="/tmp"  # destination folder to store the final iso file
-hostname="ubuntu"
+
+#Limpiamos ventana
+clear
+
+# Variables de entorno
+tmp=$(pwd)  # destination folder to store the final iso file
 currentuser="$( whoami)"
 
-# define spinner function for slow tasks
+download_file="ubuntu-16.04.1-server-amd64.iso"
+new_iso_name="ubuntu-16.04.1-server-amd64-unattended.iso"
+
+seed_file="ubuntu-asix.seed"
+
+# Definimos cola para las tareas lentas
 # courtesy of http://fitnr.com/showing-a-bash-spinner.html
 spinner()
 {
@@ -22,19 +30,7 @@ spinner()
     printf "    \b\b\b\b"
 }
 
-# define download function
-# courtesy of http://fitnr.com/showing-file-download-progress-using-wget.html
-download()
-{
-    local url=$1
-    echo -n "    "
-    wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
-        sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
-    echo -ne "\b\b\b\b"
-    echo " DONE"
-}
-
-# define function to check if program is installed
+# Comprobamos los paquetes necesarios
 # courtesy of https://gist.github.com/JamieMason/4761049
 function program_is_installed {
     # set to 1 initially
@@ -45,7 +41,7 @@ function program_is_installed {
     echo $return_
 }
 
-# print a pretty header
+# Imprimimos la pantalla de cabezera
 echo
 echo " +---------------------------------------------------+"
 echo " |            UNATTENDED UBUNTU ISO MAKER            |"
@@ -58,32 +54,6 @@ if [ $currentuser != "root" ]; then
     exit 1
 fi
 
-# ask whether to include vmware tools or not
-while true; do
-    echo " which ubuntu edition would you like to remaster:"
-    echo
-    echo "  [1] Ubuntu 12.04.4 LTS Server amd64 - Precise Pangolin"
-    echo "  [2] Ubuntu 14.04.4 LTS Server amd64 - Trusty Tahr"
-    echo "  [3] Ubuntu 16.04.1 LTS Server amd64 - Xenial Xerus"
-    echo
-    read -p " please enter your preference: [1|2|3]: " ubver
-    case $ubver in
-        [1]* )  download_file="ubuntu-12.04.4-server-amd64.iso"           # filename of the iso to be downloaded
-                download_location="http://releases.ubuntu.com/12.04/"     # location of the file to be downloaded
-                new_iso_name="ubuntu-12.04.4-server-amd64-unattended.iso" # filename of the new iso file to be created
-                break;;
-        [2]* )  download_file="ubuntu-14.04.4-server-amd64.iso"             # filename of the iso to be downloaded
-                download_location="http://releases.ubuntu.com/14.04/"     # location of the file to be downloaded
-                new_iso_name="ubuntu-14.04.4-server-amd64-unattended.iso"   # filename of the new iso file to be created
-                break;;
-        [3]* )  download_file="ubuntu-16.04.1-server-amd64.iso"
-                download_location="http://releases.ubuntu.com/16.04/"
-                new_iso_name="ubuntu-16.04.1-server-amd64-unattended.iso"
-                break;;
-        * ) echo " please answer [1], [2] or [3]";;
-    esac
-done
-
 if [ -f /etc/timezone ]; then
   timezone=`cat /etc/timezone`
 elif [ -h /etc/localtime]; then
@@ -93,45 +63,25 @@ else
   timezone=`find /usr/share/zoneinfo/ -type f -exec md5sum {} \; | grep "^$checksum" | sed "s/.*\/usr\/share\/zoneinfo\///" | head -n 1`
 fi
 
-# ask the user questions about his/her preferences
+# Realizamos las preguntas para la configuración de 'SEED'
+read -ep " please enter your preferred hostname: " -i "asixsrv01" hostname
 read -ep " please enter your preferred timezone: " -i "${timezone}" timezone
-read -ep " please enter your preferred username: " -i "netson" username
+read -ep " please enter your preferred username: " -i "administrador" username
 read -sp " please enter your preferred password: " password
 printf "\n"
 read -sp " confirm your preferred password: " password2
 printf "\n"
-read -ep " Make ISO bootable via USB: " -i "yes" bootable
 
-# check if the passwords match to prevent headaches
+# Comprobamos que el password sea correcto (Repetido)
 if [[ "$password" != "$password2" ]]; then
     echo " your passwords do not match; please restart the script and try again"
     echo
     exit
 fi
 
-# download the ubunto iso
 cd $tmp
-if [[ ! -f $tmp/$download_file ]]; then
-    echo -n " downloading $download_file: "
-    download "$download_location$download_file"
-fi
-if [[ ! -f $tmp/$download_file ]]; then
-	echo "Error: Failed to download ISO: $download_location$download_file"
-	echo "This file may have moved or may no longer exist."
-	echo
-	echo "You can download it manually and move it to $tmp/$download_file"
-	echo "Then run this script again."
-	exit 1
-fi
 
-# download netson seed file
-seed_file="netson.seed"
-if [[ ! -f $tmp/$seed_file ]]; then
-    echo -n " downloading $seed_file: "
-    download "https://raw.githubusercontent.com/netson/ubuntu-unattended/master/$seed_file"
-fi
-
-# install required packages
+# Instalamos los paquetes 'requeridos' (necesarios)
 echo " installing required packages"
 if [ $(program_is_installed "mkpasswd") -eq 0 ] || [ $(program_is_installed "mkisofs") -eq 0 ]; then
     (apt-get -y update > /dev/null 2>&1) &
@@ -139,83 +89,70 @@ if [ $(program_is_installed "mkpasswd") -eq 0 ] || [ $(program_is_installed "mki
     (apt-get -y install whois genisoimage > /dev/null 2>&1) &
     spinner $!
 fi
-if [[ $bootable == "yes" ]] || [[ $bootable == "y" ]]; then
-    if [ $(program_is_installed "isohybrid") -eq 0 ]; then
-        (apt-get -y install syslinux > /dev/null 2>&1) &
-        spinner $!
-    fi
-fi
 
-
-# create working folders
+# Creamos los directorios de trabajo
 echo " remastering your iso file"
 mkdir -p $tmp
 mkdir -p $tmp/iso_org
 mkdir -p $tmp/iso_new
 
-# mount the image
+# Montamos la imagen
 if grep -qs $tmp/iso_org /proc/mounts ; then
     echo " image is already mounted, continue"
 else
     (mount -o loop $tmp/$download_file $tmp/iso_org > /dev/null 2>&1)
 fi
 
-# copy the iso contents to the working directory
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# PROCESO DE MODIFICACIÓN DESATENDIDA!!!
+
+# Copiamos la iso en el directorio
 (cp -rT $tmp/iso_org $tmp/iso_new > /dev/null 2>&1) &
 spinner $!
 
-# set the language for the installation menu
+# Seteamos el lenguaje en el instalador
 cd $tmp/iso_new
 echo en > $tmp/iso_new/isolinux/lang
+cat $tmp/iso_new/isolinux/lang
 
-# set late command
-late_command="chroot /target wget -O /home/$username/start.sh https://github.com/netson/ubuntu-unattended/raw/master/start.sh ;\
-    chroot /target chmod +x /home/$username/start.sh ;"
-
-# copy the netson seed file to the iso
+# Copiamos el archivo SEED a la nueva ISO
 cp -rT $tmp/$seed_file $tmp/iso_new/preseed/$seed_file
 
-# include firstrun script
-echo "
-# setup firstrun script
-d-i preseed/late_command                                    string      $late_command" >> $tmp/iso_new/preseed/$seed_file
-
-# generate the password hash
+# Generamos la contraseña (encriptada)
 pwhash=$(echo $password | mkpasswd -s -m sha-512)
 
-# update the seed file to reflect the users' choices
-# the normal separator for sed is /, but both the password and the timezone may contain it
-# so instead, I am using @
+# Actualizamos el fichero SEED con el comando 'sed'
+# En base a las variables (preguntas) asignadas anteriormente)
 sed -i "s@{{username}}@$username@g" $tmp/iso_new/preseed/$seed_file
 sed -i "s@{{pwhash}}@$pwhash@g" $tmp/iso_new/preseed/$seed_file
 sed -i "s@{{hostname}}@$hostname@g" $tmp/iso_new/preseed/$seed_file
 sed -i "s@{{timezone}}@$timezone@g" $tmp/iso_new/preseed/$seed_file
 
-# calculate checksum for seed file
+# Calculamos la suma de verificación del archivo
 seed_checksum=$(md5sum $tmp/iso_new/preseed/$seed_file)
 
-# add the autoinstall option to the menu
+# IMPORTANTE: Añadimos las lineas del menú  de arranque!
+sed -i "s@default install@default autoinstall@g" $tmp/iso_new/isolinux/txt.cfg
+
 sed -i "/label install/ilabel autoinstall\n\
-  menu label ^Autoinstall NETSON Ubuntu Server\n\
+  menu label ^Autoinstall Ubuntu Server\n\
   kernel /install/vmlinuz\n\
-  append file=/cdrom/preseed/ubuntu-server.seed initrd=/install/initrd.gz auto=true priority=high preseed/file=/cdrom/preseed/netson.seed preseed/file/checksum=$seed_checksum --" $tmp/iso_new/isolinux/txt.cfg
+  append file=/cdrom/preseed/ubuntu-server.seed initrd=/install/initrd.gz auto=true priority=critical preseed/file=/cdrom/preseed/ubuntu-asix.seed preseed/file/checksum=$seed_checksum --\n" $tmp/iso_new/isolinux/txt.cfg
+
+# IMPORTANTE: Añadimos 'timeout' en isolinux para arranque automático
+sed -i "s@timeout 0@timeout 1@g" $tmp/iso_new/isolinux/isolinux.cfg
 
 echo " creating the remastered iso"
 cd $tmp/iso_new
-(mkisofs -D -r -V "NETSON_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp/$new_iso_name . > /dev/null 2>&1) &
+(mkisofs -D -r -V "UNATTENDED_UBUNTU" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o $tmp/$new_iso_name . > /dev/null 2>&1) &
 spinner $!
 
-# make iso bootable (for dd'ing to  USB stick)
-if [[ $bootable == "yes" ]] || [[ $bootable == "y" ]]; then
-    isohybrid $tmp/$new_iso_name
-fi
-
-# cleanup
+# Desmontamos iso y eliminamos directorios de trabajo
 umount $tmp/iso_org
 rm -rf $tmp/iso_new
 rm -rf $tmp/iso_org
 
-# print info to user
+# Mostramos información (resumen) de usuario
 echo " -----"
 echo " finished remastering your ubuntu iso file"
 echo " the new file is located at: $tmp/$new_iso_name"
@@ -225,14 +162,13 @@ echo " your hostname is: $hostname"
 echo " your timezone is: $timezone"
 echo
 
-# unset vars
+# Libreramos variables
 unset username
 unset password
 unset hostname
 unset timezone
 unset pwhash
 unset download_file
-unset download_location
 unset new_iso_name
 unset tmp
 unset seed_file
